880e1172ee9629b13defc1847d35bbe5affbe58c
aj07 <aktjha@gmail.com>
2021-01-19 04:50:29 +0800

    zfd

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'neardapp.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'zeggotest.testnet'
 
 function getConfig(env) {
   switch (env) {
