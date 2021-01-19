77970637984d4fa3726da04b66a6cad3716206e4
aj07 <aktjha@gmail.com>
2021-01-20 00:42:49 +0800

    update all things

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -85715,7 +85715,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36921" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43895" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
